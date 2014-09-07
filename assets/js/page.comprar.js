0d844bfa3142aa2be86a88e249bddc91b5356ce5
Arthur Freitas <arthur@shipe.me>
2014-09-07 18:51:28 -0300

    iupa

--- assets/js/page.comprar.js
+++ assets/js/page.comprar.js
@@ -7,10 +7,19 @@ jQuery('#number').mask('999999');
 
 // Exibindo ou ocultando campos de boleto.
 jQuery('[name="payment-method"]').click(function() {
-	if($('[id="payment-method-boleto"]').is(':checked')) {
+	if(jQuery('[id="payment-method-boleto"]').is(':checked')) {
 		jQuery('#fields-payment-method').removeClass('hidden');
 	} else {
 		jQuery('#fields-payment-method').addClass('hidden');
+	}
+});
+
+// Exibindo ou ocultando campo da carteira.
+jQuery('[name="has-wallet"]').click(function() {
+	if(jQuery('#has-wallet-yes').is(':checked')) {
+		jQuery('.fields-has-wallet').removeClass('hidden');
+	} else {
+		jQuery('.fields-has-wallet').addClass('hidden');
 	}
 });
 
@@ -24,6 +33,50 @@ jQuery('#bitcoin-value').keyup(function() {
 });
 
 // Função de validação do formulário.
-function validateForm() {
-	var fullname = document.forms["compre-bitcoins"]["full-name"].value;
-}
+jQuery('#full-name').focusout(function() {
+	if(jQuery(this).val() == null || jQuery(this).val() == '') {
+		jQuery(this).addClass('error');
+		jQuery('#full-name-error').html('Este campo é de preenchimento obrigatório.');
+	} else {
+		if(jQuery(this).hasClass('error')) {
+			jQuery(this).removeClass('error');
+		}
+		jQuery('#full-name-error').html(' ');
+	}
+});
+
+jQuery('#cpf').focusout(function() {
+	if(jQuery(this).val() == null || jQuery(this).val() == '') {
+		jQuery(this).addClass('error');
+		jQuery('#cpf-error').html('Este campo é de preenchimento obrigatório.');
+	} else {
+		if(jQuery(this).hasClass('error')) {
+			jQuery(this).removeClass('error');
+		}
+		jQuery('#cpf-error').html(' ');
+	}
+});
+
+jQuery('#email').focusout(function() {
+	if(jQuery(this).val() == null || jQuery(this).val() == '') {
+		jQuery(this).addClass('error');
+		jQuery('#email-error').html('Este campo é de preenchimento obrigatório.');
+	} else {
+		if(jQuery(this).hasClass('error')) {
+			jQuery(this).removeClass('error');
+		}
+		jQuery('#email-error').html(' ');
+	}
+});
+
+jQuery('#bitcoin-value').focusout(function() {
+	if(jQuery(this).val() == null || jQuery(this).val() == '') {
+		jQuery(this).addClass('error');
+		jQuery('#bitcoin-error').html('Este campo é de preenchimento obrigatório.');
+	} else {
+		if(jQuery(this).hasClass('error')) {
+			jQuery(this).removeClass('error');
+		}
+		jQuery('#bitcoin-error').html(' ');
+	}
+});
